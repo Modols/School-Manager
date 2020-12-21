217e616f10c81fd395fe9460731bede03260c447
Modolo Thomas <thomas.modolo@ynov.com>
2020-12-21 17:06:57 +0100

    adding footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,19 @@
         </h1>
         <br>
         @yield('content')
-    </div>
+    </div><br>
+    <footer class="footer mt-auto py-3 bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <span class="text-light bg-dark">© School Manager created by Modolo Thomas </span>
+                </div>
+                <div class="col-4">
+                    <span class="text-light">Ynov Toulouse Campus</span>
+                </div>
+            </div>
+        </div>
+      </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>
